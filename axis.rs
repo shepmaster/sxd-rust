db65c5543c2406478c2e01c3b2f7150115f92ba5
Jake Goulding <jake.goulding@gmail.com>
2014-08-09 16:31:08 -0400

    Move all the axes back to one file

---
+++ axis.rs
@@ -0,0 +1,94 @@
+use super::XPathEvaluationContext;
+use super::XPathNodeTest;
+use super::Nodeset;
+
+enum PrincipalNodeType {
+  Attribute,
+  Element,
+}
+
+/// A directed traversal of Nodes.
+trait XPathAxis {
+    /// Applies the given node test to the nodes selected by this axis,
+    /// adding matching nodes to the nodeset.
+    fn select_nodes(&self,
+                    context:   &XPathEvaluationContext,
+                    node_test: &XPathNodeTest,
+                    result:    &mut Nodeset);
+
+    /// Describes what node type is naturally selected by this axis.
+    fn principal_node_type() -> PrincipalNodeType {
+        Element
+    }
+}
+
+pub struct AxisAttribute;
+
+impl XPathAxis for AxisAttribute {
+    fn select_nodes(&self,
+                    context:   &XPathEvaluationContext,
+                    node_test: &XPathNodeTest,
+                    result:    &mut Nodeset)
+    {
+        for attr in context.node().attributes() {
+            let attr_context = context.new_context_for(1);
+            attr_context.next(attr);
+
+            node_test.test(&attr_context, result);
+        }
+    }
+
+    fn principal_node_type() -> PrincipalNodeType {
+        Attribute
+    }
+}
+
+pub struct AxisChild;
+
+impl XPathAxis for AxisChild {
+    fn select_nodes(&self,
+                    context:   &XPathEvaluationContext,
+                    node_test: &XPathNodeTest,
+                    result:    &mut Nodeset)
+    {
+        for child in context.node().children() {
+            let child_context = context.new_context_for(1);
+            child_context.next(child);
+
+            node_test.test(&child_context, result);
+        }
+    }
+}
+
+pub struct AxisDescendant;
+
+impl XPathAxis for AxisDescendant {
+    fn select_nodes(&self,
+                    context:   &XPathEvaluationContext,
+                    node_test: &XPathNodeTest,
+                    result:    &mut Nodeset)
+    {
+        for child in context.node().children() {
+            let child_context = context.new_context_for(1);
+            child_context.next(child);
+
+            node_test.test(&child_context, result);
+            self.select_nodes(&child_context, node_test, result);
+        }
+    }
+}
+
+pub struct AxisDescendantOrSelf {
+    descendant: AxisDescendant,
+}
+
+impl XPathAxis for AxisDescendantOrSelf {
+    fn select_nodes(&self,
+                    context:   &XPathEvaluationContext,
+                    node_test: &XPathNodeTest,
+                    result:    &mut Nodeset)
+    {
+        node_test.test(context, result);
+        self.descendant.select_nodes(context, node_test, result);
+    }
+}
