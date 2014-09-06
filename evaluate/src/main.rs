33029f440f9be98f2460fcaf7c2d87defac902e9
Jake Goulding <jake.goulding@gmail.com>
2014-09-06 13:53:11 -0400

    Update evaluate tool to parse a file

--- evaluate/src/main.rs
+++ evaluate/src/main.rs
@@ -1,9 +1,12 @@
 extern crate document;
 extern crate xpath;
 
+use std::io::File;
+
 use std::collections::hashmap::HashMap;
 
-use document::{Document,ToAny};
+use document::ToAny;
+use document::parser::Parser;
 
 use xpath::{XPathEvaluationContext,XPathFactory};
 use xpath::expression::XPathExpression;
@@ -11,20 +14,38 @@ use xpath::expression::XPathExpression;
 fn main() {
     let mut args = std::os::args();
 
-    let arg = match args.remove(1) {
+    let filename = match args.remove(1) {
+        Some(x) => x,
+        None => { println!("File required"); return; },
+    };
+
+    let xpath_str = match args.remove(1) {
         Some(x) => x,
         None => { println!("XPath required"); return; },
     };
 
     let factory = XPathFactory::new();
 
-    let expr = match factory.build(arg.as_slice()) {
+    let expr = match factory.build(xpath_str.as_slice()) {
         Err(x) => { println!("Unable to compile XPath: {}", x); return; },
         Ok(None) => { println!("Unable to compile XPath"); return; },
         Ok(Some(x)) => x,
     };
 
-    let d = Document::new();
+    let p = Parser::new();
+
+    let path = Path::new(filename);
+    let mut file = File::open(&path);
+
+    let data = match file.read_to_end() {
+        Ok(x) => String::from_utf8(x),
+        Err(x) => { println!("Can't read: {}", x); return; },
+    };
+
+    let data = data.ok().expect("Unable to convert to UTF-8");
+
+    let d = p.parse(data.as_slice());
+
     let mut functions = HashMap::new();
     xpath::function::register_core_functions(& mut functions);
     let variables = HashMap::new();
